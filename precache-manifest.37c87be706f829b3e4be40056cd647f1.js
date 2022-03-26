ce778dcd6ee94d62322b1af9d4052400d128518a
reozmen <recepozmen41@gmail.com>
2022-03-27 02:31:42 +0300

    Updates

---
+++ precache-manifest.37c87be706f829b3e4be40056cd647f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d26e217f7d158bc0a00ba06c0c7012",
+    "url": "/Accordion-project-react/index.html"
+  },
+  {
+    "revision": "17305e1cc0fe28aa5780",
+    "url": "/Accordion-project-react/static/css/main.5b1f5cfa.chunk.css"
+  },
+  {
+    "revision": "cf8f508b399a28d0bf44",
+    "url": "/Accordion-project-react/static/js/2.4ef4a036.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Accordion-project-react/static/js/2.4ef4a036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17305e1cc0fe28aa5780",
+    "url": "/Accordion-project-react/static/js/main.b54563ba.chunk.js"
+  },
+  {
+    "revision": "32974553dd2324b80ef9",
+    "url": "/Accordion-project-react/static/js/runtime-main.5497ec49.js"
+  },
+  {
+    "revision": "6ca1d62d104d7159995a3cc4e6faf0bc",
+    "url": "/Accordion-project-react/static/media/react.6ca1d62d.png"
+  }
+]);
