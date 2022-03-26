e0b616636573bd194f150081af0fb5814699d30f
reozmen <recepozmen41@gmail.com>
2022-03-27 02:30:54 +0300

    accordion project completed

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is React and why use it?',
+    info:
+      'React. js is an open-source JavaScript library that is used for building user interfaces specifically for single-page applications. It is used for handling the view layer for web and mobile apps. React also allows us to create reusable UI components.',
+  },
+  {
+    id: 2,
+    title: 'What are the features of React?',
+    info:
+      'JSX (JavaScript Syntax Extension), Virtual DOM, One-way data binding,  Performance, Extensions, Conditional statements, Components, Simplicity',
+  },
+  {
+    id: 3,
+    title: 'What is component in React?',
+    info:
+      'Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML. Components come in two types, Class components and Function components, in this tutorial we will concentrate on Function components.',
+  },
+  {
+    id: 4,
+    title: 'What is props in React?',
+    info:
+      'Props are arguments passed into React components. Props are passed to components via HTML attributes. props stands for properties.',
+  },
+  {
+    id: 5,
+    title: 'What is state in React?',
+    info:
+      'React components has a built-in state object. The state object is where you store property values that belongs to the component. When the state object changes, the component re-renders.',
+  },
+  
+]
+export default questions
